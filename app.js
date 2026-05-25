50a1b6dfb1d41fb2c122aadade180e227100e469
Priyanca Tatachari <ptatach@gmail.com>
2026-05-25 13:54:09 -0400

    port change

--- app.js
+++ app.js
@@ -10,18 +10,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-/* GLOBAL EJS VARIABLES */
-app.use((req, res, next) => {
-    res.locals.currentPath = req.path;
-    next();
-});
-
 
 // Routes
 const indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
 
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
